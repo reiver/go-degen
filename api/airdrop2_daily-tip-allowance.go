b1cb39e82ebd10cb86b14c19d1a34b29480932dd
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-21 13:41:30 -0800

    initial commits

---
+++ api/airdrop2_daily-tip-allowance.go
@@ -0,0 +1,17 @@
+package degenapi
+
+import (
+	"sourcecode.social/reiver/go-opt"
+)
+
+type ApiDrop2DailyTipAllowance struct {
+	FID                opt.Optional[string]
+	SnapshotDate       opt.Optional[string]
+	UserRank           opt.Optional[string]
+	WalletAddress      opt.Optional[string]
+	AvatarURL          opt.Optional[string]
+	DisplayName        opt.Optional[string]
+	ReactionsPerCast   opt.Optional[string]
+	TipAllowance       opt.Optional[string]
+	RemainingAllowance opt.Optional[string]
+}
