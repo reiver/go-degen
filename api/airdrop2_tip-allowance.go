e0720d315c859c1f8dfcd11d956cc298337c6496
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-21 13:42:13 -0800

    initial commits

---
+++ api/airdrop2_tip-allowance.go
@@ -0,0 +1,181 @@
+package degenapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"sourcecode.social/reiver/go-errhttp"
+	"sourcecode.social/reiver/go-opt"
+
+	"github.com/reiver/go-degen/api/url"
+)
+
+// GetAirDrop2TipAllowanceUsingWalletAddress calls the following degen API end-point:
+//
+//	/api/airdrop2/tip-allowance?address=<wallet>
+func GetAirDrop2TipAllowanceUsingWalletAddress(dailyTipAllowance *ApiDrop2DailyTipAllowance, walletAddress string) error {
+	if nil == dailyTipAllowance {
+		return errNilTipAllowance
+	}
+
+	var url string = degenapiurl.AirDrop2TipAllowanceUsingWalletAddress(walletAddress)
+
+	resp, err := http.Get(url)
+	if nil != err {
+		return err
+	}
+	if nil == resp {
+		return errNilHTTPResponse
+	}
+
+	return getAirDrop2TipAllowance(dailyTipAllowance, resp)
+}
+
+// GetAirDrop2TipPointsUsingFarcasterID calls the following degen API end-point:
+//
+//	/api/airdrop2/tip-allowance?fid=<Farcaster ID>
+func GetAirDrop2TipAllowanceUsingFarcasterID(dailyTipAllowance *ApiDrop2DailyTipAllowance,farcasterID string) error {
+	if nil == dailyTipAllowance {
+		return errNilTipAllowance
+	}
+
+	var url string = degenapiurl.AirDrop2TipAllowanceUsingFarcasterID(farcasterID)
+
+	resp, err := http.Get(url)
+	if nil != err {
+		return err
+	}
+	if nil == resp {
+		return errNilHTTPResponse
+	}
+
+	return getAirDrop2TipAllowance(dailyTipAllowance, resp)
+}
+
+func getAirDrop2TipAllowance(dailyTipAllowance *ApiDrop2DailyTipAllowance, resp *http.Response) error {
+
+	if http.StatusOK != resp.StatusCode {
+		return errhttp.Return(resp.StatusCode)
+	}
+
+	var p []byte
+	{
+		var readcloser io.ReadCloser = resp.Body
+		if nil == readcloser {
+			return errNilReadCloser
+		}
+		defer readcloser.Close()
+
+		var err error
+
+		p, err = io.ReadAll(readcloser)
+		if nil != err {
+			return err
+		}
+		readcloser.Close()
+	}
+
+	return putApiDrop2TipAllowance(dailyTipAllowance, p)
+}
+
+func putApiDrop2TipAllowance(dailyTipAllowance *ApiDrop2DailyTipAllowance, p []byte) error {
+	if nil == dailyTipAllowance {
+		return errNilTipAllowance
+	}
+
+	var result []map[string]string = []map[string]string{}
+
+	err := json.Unmarshal(p, &result)
+	if nil != err {
+		return err
+	}
+
+	if len(result) < 1 {
+		return nil
+	}
+	var result0 map[string]string = result[0]
+
+	{
+		const name string = "fid"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.FID = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "snapshot_date"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.SnapshotDate = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "user_rank"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.UserRank = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "wallet_address"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.WalletAddress = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "avatar_url"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.AvatarURL = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "display_name"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.DisplayName = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "reactions_per_cast"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.ReactionsPerCast = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "tip_allowance"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.TipAllowance = opt.Something(value)
+		}
+	}
+
+	{
+		const name string = "remaining_allowance"
+
+		value, found := result0[name]
+		if found {
+			dailyTipAllowance.RemainingAllowance = opt.Something(value)
+		}
+	}
+
+	return nil
+}
---
+++ api/airdrop2_tip-allowance_test.go
@@ -0,0 +1,258 @@
+package degenapi
+
+import (
+	"testing"
+
+	"sourcecode.social/reiver/go-opt"
+)
+
+func TestPutApiDrop2TipAllowance(t *testing.T) {
+
+	tests := []struct{
+		JSON string
+		Expected ApiDrop2DailyTipAllowance
+	}{
+		{
+			JSON: "[]",
+			Expected: ApiDrop2DailyTipAllowance{},
+		},
+
+
+
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`,`+
+				`"avatar_url":"https://example.com/bN604M3.jpeg"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+				AvatarURL: opt.Something("https://example.com/bN604M3.jpeg"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`,`+
+				`"avatar_url":"https://example.com/bN604M3.jpeg"`+
+				`,`+
+				`"display_name":"Joe Blow"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+				AvatarURL: opt.Something("https://example.com/bN604M3.jpeg"),
+				DisplayName: opt.Something("Joe Blow"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`,`+
+				`"avatar_url":"https://example.com/bN604M3.jpeg"`+
+				`,`+
+				`"display_name":"Joe Blow"`+
+				`,`+
+				`"tip_allowance":"326"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+				AvatarURL: opt.Something("https://example.com/bN604M3.jpeg"),
+				DisplayName: opt.Something("Joe Blow"),
+				TipAllowance: opt.Something("326"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`,`+
+				`"avatar_url":"https://example.com/bN604M3.jpeg"`+
+				`,`+
+				`"display_name":"Joe Blow"`+
+				`,`+
+				`"tip_allowance":"326"`+
+				`,`+
+				`"remaining_allowance":"26"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+				AvatarURL: opt.Something("https://example.com/bN604M3.jpeg"),
+				DisplayName: opt.Something("Joe Blow"),
+				TipAllowance: opt.Something("326"),
+				RemainingAllowance: opt.Something("26"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"fid":"269231"`+
+				`,`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`,`+
+				`"avatar_url":"https://example.com/bN604M3.jpeg"`+
+				`,`+
+				`"display_name":"Joe Blow"`+
+				`,`+
+				`"tip_allowance":"326"`+
+				`,`+
+				`"remaining_allowance":"26"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				FID: opt.Something("269231"),
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+				AvatarURL: opt.Something("https://example.com/bN604M3.jpeg"),
+				DisplayName: opt.Something("Joe Blow"),
+				TipAllowance: opt.Something("326"),
+				RemainingAllowance: opt.Something("26"),
+			},
+		},
+		{
+			JSON:
+				`[`+
+				`{`+
+				`"fid":"269231"`+
+				`,`+
+				`"snapshot_date":"2024-02-21T00:00:00.000Z"`+
+				`,`+
+				`"user_rank":"25789"`+
+				`,`+
+				`"wallet_address":"0xfecdab9876543210123456789abcdefedcba9876"`+
+				`,`+
+				`"avatar_url":"https://example.com/bN604M3.jpeg"`+
+				`,`+
+				`"display_name":"Joe Blow"`+
+				`,`+
+				`"reactions_per_cast":"1"`+
+				`,`+
+				`"tip_allowance":"326"`+
+				`,`+
+				`"remaining_allowance":"26"`+
+				`}`+
+				`]`,
+			Expected: ApiDrop2DailyTipAllowance{
+				FID: opt.Something("269231"),
+				SnapshotDate: opt.Something("2024-02-21T00:00:00.000Z"),
+				UserRank: opt.Something("25789"),
+				WalletAddress: opt.Something("0xfecdab9876543210123456789abcdefedcba9876"),
+				AvatarURL: opt.Something("https://example.com/bN604M3.jpeg"),
+				DisplayName: opt.Something("Joe Blow"),
+				ReactionsPerCast: opt.Something("1"),
+				TipAllowance: opt.Something("326"),
+				RemainingAllowance: opt.Something("26"),
+			},
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		var p []byte = []byte(test.JSON)
+
+		var actual ApiDrop2DailyTipAllowance
+
+		err := putApiDrop2TipAllowance(&actual, p)
+
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one." , testNumber)
+			t.Logf("ERROR: (%T) %s", err, err)
+			t.Logf("JSON:\n%s", test.JSON)
+			return
+		}
+
+		{
+			expected := test.Expected
+
+			if expected != actual {
+				t.Errorf("For test #%d, the actual airdrop2 daily-tip-allowance is not what was expected.", testNumber)
+				t.Logf("EXPECTED: %v", expected)
+				t.Logf("ACTUAL:   %v", actual)
+				t.Logf("JSON:\n%s", test.JSON)
+				continue
+			}
+		}
+	}
+}
