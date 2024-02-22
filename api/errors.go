dbba811c274c4046e441a8239a9450ade0fa6b79
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-21 16:09:28 -0800

    initial commits

--- api/errors.go
+++ api/errors.go
@@ -6,6 +6,7 @@ import (
 
 const (
 	errNilHTTPResponse = erorr.Error("degen: nil HTTP-response")
+	errNilPoints       = erorr.Error("degen: nil points")
 	errNilReadCloser   = erorr.Error("degen: nil read-closer")
 	errNilTipAllowance = erorr.Error("degen: nil tip-allowance")
 )
